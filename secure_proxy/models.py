5d97585068872f8b55191bceae018a485a5b6d20
Oleg Mingalev <oleg@mingalev.net>
2019-03-14 23:05:09 +0300

    get_content typo

--- secure_proxy/models.py
+++ secure_proxy/models.py
@@ -79,7 +79,7 @@ class Cacher(models.Model):
 
         cached_page = cacher_cache.get(str(self.uuid))
 
-        if cacher_cache is None:
+        if cacher_page is None:
             cacher_page = cacher_cache.get_or_set(
                 str(self.uuid),
                 self.go_url(),
